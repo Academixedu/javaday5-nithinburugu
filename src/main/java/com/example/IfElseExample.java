00da0a6d76a28684ecbc5d86a0b3650ac8f1a695
nithinburugu <burugunithin03@gmail.com>
2024-05-31 10:14:54 +0530

    commit

--- src/main/java/com/example/IfElseExample.java
+++ src/main/java/com/example/IfElseExample.java
@@ -3,7 +3,7 @@ package com.example;
 public class IfElseExample {
     public static void main(String[] args) {
         int number = 10;
-        if (number < 0) {
+        if (number > 0) {
             System.out.println("Number is positive");
         } else {
             System.out.println("Number is negative");
